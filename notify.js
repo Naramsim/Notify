935d9e513dc2b6917252a23cf3b2c6d64ab96317
Alessandro Pezzè <igougi.ui@gmail.com>
2015-09-22 22:27:49 +0200

    Code rewriting(with classes)

--- notify.js
+++ notify.js
@@ -1,58 +1,67 @@
-toast_height = 36 + "px"
+var toast_height = 36 + "px"
 
-function createToast () {
-	var toast = document.createElement('div')
-	return toast
-}
-
-function getToast () {
-	return document.getElementById('NotifyToast')
-}
-
-function styleToast (toast) {
-	toast.setAttribute('id', 'NotifyToast')
-	toast.setAttribute('class', 'NotifyHide')
-	toast.style.backgroundColor = 'Black'
-	toast.style.width = '100%'
-	toast.style.height = toast_height
-	toast.style.position = 'fixed'
-	toast.style.position = 'fixed'
-	toast.style.bottom = "-" + toast_height
-	toast.style["-webkit-transition"] = "0.3s ease-in-out";
-	toast.style["-moz-transition"] = "0.3s ease-in-out";
-	toast.style["-o-transition"] = "0.3s ease-in-out";
-	toast.style.transition = "0.3s ease-in-out";
-}
-
-function appendToast (toast) {
-	document.body.style.margin= "0 0 0 0"
-	document.body.appendChild(toast)
-}
-
-function showToast (toast) {
-	toast.classList.remove('NotifyHide')
-	toast.style.visibility = 'visible'
-	toast.style.bottom = '0px'
-}
-
-function hideToast (toast) {
-	toast.classList.add('NotifyHide')
-	toast.style.visibility = 'hidden'
-	toast.style.bottom = "-" + toast_height
-}
-
-function startToast (timer, message) { //start all the process that show, wait and hide a toast
-	toast = getToast()
-	if ( (toast.classList.contains("NotifyHide")) ) {
-		showToast(toast)
-		setTimeout(hideToast, timer, toast)
+function Toast () {
+	
+	function createToast() { //Private
+		var toastElement = document.createElement('div')
+		return toastElement
 	}
+	function styleToast() { //Private
+		setTimeout(function() { toast.classList.add('NotifyToast'); }, 0);
+	}
+	function appendToastToBody() { //Private
+		document.body.appendChild(toast)
+	}
+	this.showToast = function() { //Public 
+		setTimeout(function() { toast.classList.add('NotifyShow'); }, 90);
+		toast.style.visibility = 'visible'
+	}
+	this.hideToast = function() { //Public 
+		toast.classList.remove('NotifyShow')
+		toast.classList.add('NotifyHide')
+		toast.style.visibility = 'hidden'
+		setTimeout(function() { document.body.removeChild(toast) }, 500);
+	}
+    
+	var toast = createToast()
+	styleToast()
+	appendToastToBody()
+
+	this.getToast = function() { // Public Getter
+        return toast;
+    }
+}
+
+function startToast(timer, message) { //start all the process that show, wait and hide a toast
+	currentToast = new Toast()
+	currentToast.showToast()
+	setTimeout(currentToast.hideToast, timer)
 }
 
 document.addEventListener('DOMContentLoaded', function(){
-	toast = createToast()
-	styleToast(toast)
-	appendToast(toast)
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = "	body { \
+							margin: 0 0 0 0; \
+						} \
+						.NotifyToast { \
+							width: 100%; \
+							height: " + toast_height + "; \
+							background-color: black; \
+							position: fixed; \
+							bottom: -" + toast_height + "; \
+							-webkit-transition :0.3s ease-in-out; \
+							-moz-transition: 0.3s ease-in-out; \
+							-o-transition: 0.3s ease-in-out; \
+							transition: 0.3s ease-in-out; \
+						} \
+						.NotifyHide { \
+							bottom: -" + toast_height + "; \
+						} \
+						.NotifyShow { \
+							bottom: 0px; \
+						}";
+	document.body.appendChild(css);
 });
 
 window.addEventListener('keydown', function(k) {
